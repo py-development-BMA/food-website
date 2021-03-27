ee1c454535359da7793fcf82ac1c0f1e52ddd57a
Misha <1mgavura@gmail.com>
2021-03-27 16:59:23 +0200

    v 1.9 added recipe page

--- main/views.py
+++ main/views.py
@@ -23,7 +23,7 @@ class bcolors:
     BOLD = '\033[1m'
     UNDERLINE = '\033[4m'
 
-print(f"{bcolors.FAIL}\n[  VERSION  ]: 1.8.4 (created page for storage by ALEXEY)\n\n{bcolors.ENDC}")
+print(f"{bcolors.FAIL}\n[  VERSION  ]: 1.9.0 (created page for storage by ALEXEY)\n\n{bcolors.ENDC}")
 
 
 def signup(request):
